7a4b770c4d97757b6f510878b03b4ca44adcb998
Brandon Schurman <brandon@earthly.dev>
2026-02-06 15:44:43 -0500

    Clean up debug logging, document LUNAR_HUB_HOST limitation

--- collectors/semgrep/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install dependencies for the semgrep collector
-
-# postgresql-client is needed for the github-app-default-branch collector
-# which queries the Lunar Hub database
-apk add --no-cache postgresql-client
